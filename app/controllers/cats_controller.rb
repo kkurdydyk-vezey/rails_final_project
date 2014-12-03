e15cdac3445857eee0590ed60252963b41f3ccb1
Kara Kurdydyk <kkurdydyk-vezey@academic.rrc.ca>
2014-12-03 12:00:00 -0600

    Added index action to cats controller.

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,6 +1,6 @@
 class CatsController < ApplicationController
   
   def index
-            
+    @cats = Cat.order(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   #GET / triggers index action of cats controller
   root 'cats#index'
-  
+  # GET /id triggers show command of cats controller
+  get 'cats/:id' => 'cats#show'
   
 end
