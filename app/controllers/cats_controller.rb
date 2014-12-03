6e945da453cf99b7d7d0bfd8505366b343cea561
Kara Kurdydyk <kkurdydyk-vezey@academic.rrc.ca>
2014-12-03 12:15:07 -0600

    Added show action/view with links from the index.

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,6 +1,10 @@
 class CatsController < ApplicationController
-  
+  # collection action to load all cats by name
   def index
     @cats = Cat.order(:name)
+  end
+  #associated view is now automatically loaded 
+  def show
+    @cat = Cat.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   #GET / triggers index action of cats controller
   root 'cats#index'
   # GET /id triggers show command of cats controller
-  get 'cats/:id' => 'cats#show'
+  # must be numeric
+  get 'cats/:id' => 'cats#show', constraints: {id: /\d+/}, as: 'cat'
   
 end
